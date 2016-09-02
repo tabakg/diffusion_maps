089dfc57df2e42a80558034e08607a8a31b5aa7e
Gil Tabak <tabak.gil@gmail.com>
2016-09-01 22:45:27 -0700

    added way to check stationary distribution against largest eigenvector of transition matrix

--- diffusion_maps.py
+++ diffusion_maps.py
@@ -11,13 +11,16 @@ import random
 
 import scipy.sparse as sparse
 from scipy.sparse.linalg import eigsh
+from scipy.sparse.linalg import eigs
 
-dim = 1
-data_size = 5000
+
+
+dim = 5
+data_size = 20
 
 mu = 0.0 ## data distribution mean.
 sigma = 1.0 ## data distribution standard deviation.
-epsilon = 0.003 ## cutoff for nearest neighbors.
+epsilon = 4.0 ## cutoff for nearest neighbors.
 gaussian_epsilon = epsilon / 3. ## width of Gaussian kernel.
 alpha = 0.5 ## coefficient to use for diffusion maps. See the wikipedia article.
 
@@ -64,6 +67,17 @@ for i,j,v in zip(K_coo.row, K_coo.col, K_coo.data):
     L[i,j] = v * np.power(d_X[i] * d_X[j], -alpha)
 if printing_calculations: print "L = ",L.todense()
 
+# ## Calculate transition matrix. Optional.
+# P = sparse.lil_matrix((data_size,data_size))
+# for i,j,v in zip(K_coo.row, K_coo.col, K_coo.data):
+#     P[i,j] = v / d_X[i]
+# if printing_calculations: print "P = ",L.todense()
+
+D_no_power = np.squeeze(np.asarray(L.sum(axis = 1))) ## get diagonal elements of D
+D_inv = np.power(D_no_power, -1.0)
+D_inv = sparse.diags([D_inv],[0],format = 'csr')
+M = D_inv * L.tocsr()
+
 D_diag = np.power(np.squeeze(np.asarray(L.sum(axis = 1))),-0.5) ## get diagonal elements of D
 D_s = sparse.diags([D_diag],[0],format = 'csr')
 if printing_calculations: print "D_s = ", D_s.todense()
@@ -73,7 +87,18 @@ if printing_calculations: print M_s.todense()
 
 t4 = time.time()
 
-e_vals,e_vecs = eigsh(M_s, k = 1)
+e_vals,e_vecs = eigsh(M_s, k = 10,maxiter = data_size * 100 )
+
+# ## Fnd biggest eigenvector for P, to compare to phi_0. Optional.
+# P_e_vals,P_e_vecs = eigs(P.T, k = 1,maxiter = data_size * 100 )
+
+def normalize(v):
+    return v / la.norm(v)
+
+# phi_0 = d_X / sum(d_X)
+# if printing_calculations: print normalize(phi_0)
+# first_vec = P_e_vecs [:,0]
+# if printing_calculations: print normalize (first_vec)
 
 t5 = time.time()
 

e4971f19b71194c14d8d337cebcb69087c2b352d
Gil Tabak <tabak.gil@gmail.com>
2016-10-09 17:15:50 -0700

    added the option to use diffusion map with number of neighbors instead of epsilon cutoff

--- diffusion_maps.py
+++ diffusion_maps.py
@@ -1,4 +1,4 @@
-## local path to vp_tree library. 
+## local path to vp_tree library.
 ## Available at https://github.com/tabakg/cpp/tree/master/vp_trees_with_python_interface
 
 import sys
@@ -22,40 +22,60 @@ def make_gaussian_data(mu,sigma,num,dim):
     return (np.reshape(np.array([random.normalvariate(mu,sigma)
         for i in range(num*dim)]),(num,dim)) .tolist())
 
-def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = False, tol = 1e-14):
+def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = False, metric = "euclidean", neighbor_selection = "epsilon_cutoff"):
     '''
-    Optional argument symmetric detemines whether to use the symmetrized M_s or the non-symmetrized version. 
+    Optional argument symmetric detemines whether to use the symmetrized M_s or the non-symmetrized version.
     The symmetrized version could improve stability/performance, etc.
-    
+
     Optional argument eig_vec_both_sides determines whether to compute both the left and right eigenvectors. If it is false, only     the right eigenvectors are computed.
-    
-    
+
+    Optional argument neighbor_selection:
+        "epsilon_cutoff" -- using some fixed epsilon.
+        "num_neighbors" -- fixed number of neighbors.
     '''
-    
-    epsilon, gaussian_epsilon,alpha,k = params["epsilon"],params["gaussian_epsilon"],params["alpha"],params["eigen_dims"]
+
+    gaussian_epsilon,alpha,k = params["gaussian_epsilon"],params["alpha"],params["eigen_dims"]
+    if neighbor_selection == "epsilon_cutoff":
+        epsilon = params["epsilon"]
+    elif neighbor_selection == "num_neighbors":
+        num_neighbors = params["num_neighbors"]
+    else:
+        raise ValueError("Unknown neighbor selection method. Please use epsilon_cutoff or num_neighbors")
+
     data_size = len(data)
 
     D = {tuple(value):i for i,value in enumerate(data)}
 
     t0 = time.time()
-    tree = vp_tree.tree_container(data) ## make vp tree from data.
+    tree = vp_tree.tree_container(data,metric) ## make vp tree from data.
     t1 = time.time()
     s = tree.print_tree() ## string with tree contents.
     t2 = time.time()
 
     #### Diffusion Mapping.
-    ## We use the default metric here.
-    ## For the FS_metric, use: vp_tree.FS_metric(x,y).
-    ## Remember to normalize x and y first!!
-    ## print vp_tree.FS_metric((x/la.norm(x)).tolist(),(y/la.norm(y)).tolist())
+
+    def fill_K_row(i,x,neighbors,K):
+        '''
+        Fill the ith row of the K similarity matrix,
+        corresponding to point x with its neighborsself.
+        '''
+        for y in neighbors:
+            j = D[tuple(y)] ## index of point y
+            K[i,j] = np.exp(-la.norm(np.asarray(x)-np.asarray(y))**2 / (2.0 * gaussian_epsilon))
+        return
 
     K = sparse.lil_matrix((data_size,data_size)) ## efficient format for constructing matrices.
+    if neighbor_selection == "epsilon_cutoff":
+        for i,x in enumerate(data):
+            neighbors = tree.find_within_epsilon(x,epsilon,metric)
+            fill_K_row(i,x,neighbors,K)
+    elif neighbor_selection == "num_neighbors":
+        print "using num_neighbors  "
+        all_neighbors = tree.find_all_N_neighbors(num_neighbors, metric)
+        for i,neighbors in enumerate(all_neighbors):
+            x = data[i]
+            fill_K_row(i,x,neighbors,K)
 
-    for i,x in enumerate(data):
-        neighbors = tree.find_within_epsilon(x,epsilon)
-        for y in neighbors:
-            j = D[tuple(y)]
-            K[i,j] = np.exp(-la.norm(np.asarray(x)-np.asarray(y))**2 / (2.0 * gaussian_epsilon))
     if printing_calculations: print "K = ", K.todense()
 
     if printing_calculations: print "total neighbors found = ", len(K.nonzero()[0])
@@ -72,7 +92,7 @@ def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = Fals
     if printing_calculations: print "L = ",L.todense()
 
     D_no_power = np.squeeze(np.asarray(L.sum(axis = 1))) ## get diagonal elements of D
-           
+
     ## symmetrized
     if symmetric:
         D_frac_pow = np.asarray([np.power(d, - 0.5) if not d == 0. else 0. for d in D_no_power])
@@ -80,18 +100,18 @@ def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = Fals
         if eig_vec_both_sides: ## This matrix gives the change of basis for the left eigenvectors.
             D_frac_pow_inv = np.asarray([np.power(d, 0.5) if not d == 0. else 0. for d in D_no_power])
             D_s_inv = sparse.diags([D_frac_pow_inv],[0],format = 'csr')
-        if printing_calculations: 
+        if printing_calculations:
             print "D_s = ", D_s.todense()
         M = D_s * L.tocsr() * D_s
-        if printing_calculations: 
+        if printing_calculations:
             print M.todense()
     else: ## non-symmetrized. Must use eigensolver that doesn't assume symmetry.
         D_frac_pow = np.asarray([np.power(d, - 1) if not d == 0. else 0. for d in D_no_power])
         D_s = sparse.diags([D_frac_pow],[0],format = 'csr')
         M = D_s * L.tocsr()
-        
+
     t4 = time.time()
-    
+
     def real_and_sorted(e_vals,e_vecs):
         ## get real part, there should not be imaginary part.
         ## Then sort the eigenvectors and eigenvalues s.t. the eigenvalues monotonically decrease.
@@ -99,14 +119,14 @@ def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = Fals
         l = zip(e_vals,e_vecs.T)
         l.sort(key = lambda z: -z[0])
         return np.asarray([el[0] for el in l]),np.asarray([el[1] for el in l]).T
-    
+
     if symmetric:
         e_vals_tmp,e_vecs_tmp = eigsh(M, k = k, maxiter = data_size * 100 )
-        ## change of basis below for right eigenvectors 
+        ## change of basis below for right eigenvectors
         e_vecs = np.asarray(D_s * np.asmatrix(e_vecs_tmp))
         e_vals,e_vecs = real_and_sorted(e_vals_tmp,e_vecs)
         if eig_vec_both_sides:
-            ## change of basis below for left eigenvectors 
+            ## change of basis below for left eigenvectors
             e_vecs_left = np.asarray(D_s_inv * np.asmatrix(e_vecs_tmp) )
             _,e_vecs_left = real_and_sorted(e_vals_tmp,e_vecs_left)
     else: ## not symmetric
@@ -115,7 +135,7 @@ def run_diffusion_map(data, params, symmetric = False, eig_vec_both_sides = Fals
         if eig_vec_both_sides:
             e_vals_left,e_vecs_left = eigs(M.T, k = k, maxiter = data_size * 100 )
             e_vals_left,e_vecs_left = real_and_sorted(e_vals_left,e_vecs_left)
-    
+
     t5 = time.time()
 
     if printing_calculations:
